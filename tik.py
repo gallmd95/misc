ff6d70cb89e9ae1dd509eeb3d2614901dd552598
mgallagher <mgallagher@conversantmedia.com>
2017-11-03 11:40:45 -0500

    Modularity

--- tik.py
+++ tik.py
@@ -5,31 +5,38 @@ from string import ascii_lowercase
 from multiprocessing import Pool
 from multiprocessing.dummy import Pool as ThreadPool 
 import time
+
+n = 3
+
 start_time = time.time()
 
 pool = ThreadPool(8) 
+
 
 def req(x):
     ur = "https://finance.yahoo.com/quote/"+x
     print ur
     try:
-        r = requests.get(ur)
+        r = requests.get(ur,allow_redirects=False)
         if r.status_code != 404 and r.text[:6] != "Moved ":
-            return r
+            return x
     except requests.exceptions.ContentDecodingError:
-        print x
+        print "error "+x
     
 
 
-syms = (''.join(i) for i in product(ascii_lowercase, repeat=2))
+syms = (''.join(i) for i in product(ascii_lowercase, repeat=n))
 
 a = pool.map(req,syms)
 pool.close() 
 pool.join() 
 print "Swim time over!"
+print len(a)
 
-with open("symbols2.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerows(a)
+f = open('symbols'+str(n)+'.txt', 'wb')
+
+for item in a:
+    if item is not None:
+        f.write("%s\n" % item)
 
 print("--- %s seconds ---" % (time.time() - start_time))
